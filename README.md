9d7b9d50f63ffed1512fea29f6a6ef62acba08a9
Alican Bircan <alican.bircan@outlook.com>
2026-02-09 18:47:56 +0100

    Added initial functionality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# typecat
-Why let computer print content in terminal when cat can translate for you?
+# typecat 🐱
+Typecat is a lightweight utility for word transformation and linguistic mapping into cat-friendly language (or lolcat). Y let computers rite nonsense in terminal when kat can tranzlate 4 u?
+
+## Installation
+
+## Usage
+
+## Sources
+The dictionary used in this project is adopted from http://www.dribin.org/dave/lolspeak/ by Dave Dribin.
+The actual file is provided by the [lolspeak](https://github.com/matthewhadley/lolspeak) project by Matthew Hadley.
--- cmd/typecat/main.go
+++ cmd/typecat/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -23,20 +27,47 @@ func main() {
 	// Check if the file exists and isn't a directory
 	info, err := os.Stat(absPath)
 	if os.IsNotExist(err) {
-		fmt.Printf("Error: File does not exist at %s\n", absPath)
+		fmt.Printf("File does not exist at %s\n", absPath)
 		return
 	}
 	if info.IsDir() {
-		fmt.Printf("Error: %s is a directory, not a file\n", absPath)
+		fmt.Printf("%s is a directory, not a file\n", absPath)
 		return
 	}
 
-	// 3. Read the file
-	data, err := os.ReadFile(absPath)
+	// Read the file
+	dataByte, err := os.ReadFile(absPath)
 	if err != nil {
 		fmt.Printf("Error reading file: %v\n", err)
 		return
 	}
+	data := string(dataByte)
 
-	fmt.Printf("File content:\n%s", string(data))
+	// Read the dictionary file as dictionary
+	dictData, err := os.ReadFile("./data/dictionary.yml")
+	if err != nil {
+		fmt.Println("Error looking up dictionary: ", err)
+		return
+	}
+	dictionary := make(map[string]string)
+
+	// Unmarshal dictionary data into a map
+	err = yaml.Unmarshal(dictData, &dictionary)
+	if err != nil {
+		fmt.Println("Error looking up dictionary: ", err)
+		return
+	}
+
+	// This regex [a-zA-Z]+ finds sequences of letters.
+	re := regexp.MustCompile(`[a-zA-Z0-9']+`)
+
+	// Replace matches while preserving everything else
+	result := re.ReplaceAllStringFunc(data, func(word string) string {
+		if replacement, exists := dictionary[strings.ToUpper(word)]; exists {
+			return replacement
+		}
+		return strings.ToUpper(word)
+	})
+
+	fmt.Printf("File content:\n%s", result)
 }
